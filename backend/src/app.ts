76ebf479a9c9858fe25d4bb69339479c271a32e8
Vansh singh sura <vanshsura29@gmail.com>
2026-09-04 23:03:30 +0530

    Add Frontend URL in cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://sdms-erp-front.onrender.com"],
     credentials: true,
   }),
 );
